4efb6b09da38f6ad805c39f927731a894a40ccc4
msbro-rmw <mdinabano189@gmail.com>
2026-04-15 22:26:35 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -216,9 +216,9 @@ async def restart_handler(_, m):
 
 
 @bot.on_message(filters.command("owner"))
-async def help_handler(client: Client, message: Message):
+async def owner_handler(client: Client, message: Message):
     db.register_user(message.from_user.id)
-    owner_text = (
+    owner_text = 
         "╔══════════════════════════╗\n"
         "║**My Owner**:@SmartBoy_ApnaMS  ║\n"
         "╚══════════════════════════╝\n\n"
